884fa971fa0ddf2157441163435a89d191b25c8c
againF <wyffcld@163.com>
2017-05-18 20:29:07 +0800

    优化用户体验，现在用户可以直接从一级菜单斜着切换到二级菜单，而不用像之前的需要先平移到二级菜单

--- js/megaDropDown.js
+++ js/megaDropDown.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
 	var sub = $('#sub');//子菜单
-	
 	var activeRow;//当前激活的一级菜单中的行
 	var activeMenu;//当前激活的菜单
+	var timer;//用来保存setTimeout返回的计时器ID
+	var mouseInSub = false;//用来标识当前鼠标在不在子菜单里
+	
+	sub.on('mouseenter', function(e) {
+		mouseInSub = true;
+	}).on('mouseleave', function(e) {
+		mouseInSub = false;
+	})
 	$('#test')//一级菜单
 	 .on('mouseenter', function(e) {//鼠标移动到一级菜单时显示二级菜单
 		sub.removeClass('none');
@@ -28,11 +35,23 @@ $(document).ready(function() {
 	 		return;
 	 	}
 	 	
-	 	activeRow.removeClass('active');
-	 	activeMenu.addClass('none');
-	 	activeRow = $(e.target);
-	 	activeRow.addClass('active');
-	 	activeMenu = $('#' + activeRow.data('id'));
-	 	activeMenu.removeClass('none');
+	 	if(timer) {//实现去抖
+	 		clearTimeout(timer);
+	 	}
+	 	
+	 	timer = setTimeout(function() {
+	 		if(mouseInSub) {
+	 			return;
+	 		}
+	 		activeRow.removeClass('active');
+	 		activeMenu.addClass('none');
+	 		activeRow = $(e.target);
+	 		activeRow.addClass('active');
+	 		activeMenu = $('#' + activeRow.data('id'));
+	 		activeMenu.removeClass('none');
+	 		timer = null;
+	 	},300);
+	 	
+	 	
 	 });
 });
